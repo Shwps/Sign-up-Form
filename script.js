c4d63207b6c58934f68171a762d8a2e0b0581559
William Ekelund <bwekelund@gmail.com>
2022-08-26 10:29:09 +0200

    Added JS for password validation

--- script.js
+++ script.js
@@ -1,37 +1,75 @@
-let password = document.querySelector("#pw");
-let confirmPassword = document.querySelector("#cpw");
+const password = document.querySelector("#pw");
+const confirmPassword = document.querySelector("#cpw");
+const btn = document.querySelector("button");
+const errorCon = document.querySelector(".errorCon");
 
-if (
-  password === document.activeElement ||
-  confirmPassword === document.activeElement
-) {
-  if (password.textContent != confirmPassword.textContent) {
+const UpperErrorMsg = document.createElement("p");
+const UpperErrorNode = document.createTextNode(
+  "Password must contain at least one uppercase letter \n"
+);
+UpperErrorMsg.appendChild(UpperErrorNode);
+
+const NumErrorMsg = document.createElement("p");
+const NumErrorNode = document.createTextNode(
+  "Password must contain at least one number"
+);
+NumErrorMsg.appendChild(NumErrorNode);
+
+const LengthErrorMsg = document.createElement("p");
+const LengthErrorNode = document.createTextNode(
+  "Password must contain at least 8 characters"
+);
+LengthErrorMsg.appendChild(LengthErrorNode);
+
+password.addEventListener("input", () => {
+  if (uppercaseValidation()) {
+    errorCon.removeChild(UpperErrorMsg);
+  } else {
+    errorCon.appendChild(UpperErrorMsg);
   }
-}
+  if (containsNumber()) {
+    errorCon.removeChild(NumErrorMsg);
+  } else {
+    errorCon.appendChild(NumErrorMsg);
+  }
+  if (lengthValidation()) {
+    errorCon.removeChild(LengthErrorMsg);
+  } else {
+    errorCon.appendChild(LengthErrorMsg);
+  }
+});
+
+confirmPassword.addEventListener("input", () => {
+  if (comparePassword() === false) {
+    console.log("not the same");
+  }
+});
 
 //Att göra:
 
 //En loop som kollar om bokstäver är uppercase
-let length = password.length;
-let pwUpper = password.toUpperCase();
 
 function uppercaseValidation() {
-  for (let i = 0; i < password.length; i++) {
-    return password[i] === pwUpper[i];
+  let pwUpper = password.value.toUpperCase();
+  for (let i = 0; i < password.value.length; i++) {
+    if (password.value[i] === pwUpper[i]) {
+      return true;
+    }
   }
+  return false;
 }
 
 //En loop som kollar om strängen innehåller någon siffra
-function containsNumber(password) {
-  return /\d/.test(password);
+function containsNumber() {
+  return /\d/.test(password.value);
 }
 
 //En kontroll som kollar att strängen minst är 8 tecken
 function lengthValidation() {
-    return 8 < password.length;
+  return 8 < password.value.length;
 }
 //En kontroll som jämför password och confirmPassword
 
-function comparePassword(){
-    return password === confirmPassword;
+function comparePassword() {
+  return password === confirmPassword;
 }
